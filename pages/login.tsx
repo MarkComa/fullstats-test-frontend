7a2946834ea4a114c8904aa27bdae1841a1610dc
markcoma <dex.go@yandex.ru>
2022-05-07 22:53:18 +0300

    begin

---
+++ pages/login.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+import Banner from '../assets/images/illustration_login.svg'
+
+const Login: NextPage = () => {
+  return <div>Login
+  </div>
+}
+
+export default Login

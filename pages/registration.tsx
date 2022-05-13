f550b9dcc82c49583b4b14a1fb839aed1a7c99ad
markcoma <dex.go@yandex.ru>
2022-05-13 23:20:19 +0300

    registration page stylezed, beginning styling resetPassword page

--- pages/registration.tsx
+++ pages/registration.tsx
@@ -13,46 +13,48 @@ const Registration: NextPage = () => {
 		<div className={s.registration}>
 			<AuthBanner banner={banner.src} />
 			<div>
-				<header>
+				<header className={s.header}>
 					<span>Уже есть аккаунт?</span>
 					<Link href='/login'>Войти</Link>
 				</header>
-				<h2>Зарегистрироваться</h2>
-				<p>Зарегистрируйтесь, чтобы пользоваться сервисом:</p>
-				<form onSubmit={handleSubmit(onSubmit)} className={s.form}>
-					<input
-						type='text'
-						className={s.firstname}
-						placeholder='Имя'
-						{...register("firstname", { required: true })}
-					/>
-					<input
-						type='text'
-						className={s.surname}
-						placeholder='Фамилия'
-						{...register("surname", { required: true })}
-					/>
-					<input
-						type='email'
-						className={s.email}
-						placeholder='Почта'
-						{...register("email", { required: true })}
-					/>
-					<input
-						type='password'
-						className={s.password}
-						placeholder='Пароль'
-						{...register("password", { required: true })}
-					/>
-					<Button type='submit' apparance={"primary"}>
-						Зарегистрироваться
-					</Button>
-				</form>
-				<span>
-					Регистрируясь, я соглашаюсь с минимальными условиями
-					обслуживания и
-					<Link href='#'>Политикой конфиденциальности.</Link>
-				</span>
+				<div className={s.hero}>
+					<h2 className={s.title}>Зарегистрироваться</h2>
+					<p>Зарегистрируйтесь, чтобы пользоваться сервисом:</p>
+					<form onSubmit={handleSubmit(onSubmit)} className={s.form}>
+						<input
+							type='text'
+							className={s.firstname}
+							placeholder='Имя'
+							{...register("firstname", { required: true })}
+						/>
+						<input
+							type='text'
+							className={s.surname}
+							placeholder='Фамилия'
+							{...register("surname", { required: true })}
+						/>
+						<input
+							type='email'
+							className={s.email}
+							placeholder='Почта'
+							{...register("email", { required: true })}
+						/>
+						<input
+							type='password'
+							className={s.password}
+							placeholder='Пароль'
+							{...register("password", { required: true })}
+						/>
+						<Button type='submit' apparance={"primary"} className={s.btn}>
+							Зарегистрироваться
+						</Button>
+					</form>
+					<span className={s.privacy}>
+						{`Регистрируясь, я соглашаюсь с минимальными условиями
+						обслуживания и `}	
+						<Link href='#'>Политикой конфиденциальности.</Link>
+					</span>
+				</div>
 			</div>
 		</div>
 	);
--- pages/resetPassword.tsx
+++ pages/resetPassword.tsx
@@ -2,18 +2,19 @@ import { NextPage } from 'next'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { Button } from '../components/Button/Button';
+import s from '../styles/ResetPassword.module.scss'
 
 const ResetPassword: NextPage = () => {
   const {register,handleSubmit, formState: {errors}} = useForm();
   const onSubmit = () => {}
   return (
-    <div>
-      <h2>Забыли свой пароль?</h2>
-      <p>Пожалуйста, введите адрес электронной почты, связанный с вашей учетной записью, и мы отправим вам ссылку для сброса пароля.</p>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="email" />
-        <Button type='submit' apparance={'primary'}>Востанновить пароль</Button>
-        <Button apparance='transparent' onClick={() => history.back()} color='blue'>Назад</Button>
+    <div className={s.resetPassword}>
+      <h2 className={s.title}>Забыли свой пароль?</h2>
+      <p className={s.description}>Пожалуйста, введите адрес электронной почты, связанный с вашей учетной записью, и мы отправим вам ссылку для сброса пароля.</p>
+      <form onSubmit={handleSubmit(onSubmit)} className={s.form}>
+        <input type="email" {...register('email', {required: true})} className={s.input} placeholder={'Email'}/>
+        <Button className={s.btn} type='submit' apparance={'primary'}>Востанновить пароль</Button>
+        <Button className={s.btn__back} apparance='transparent' onClick={() => history.back()} color='blue'>Назад</Button>
       </form>
     </div>
   )

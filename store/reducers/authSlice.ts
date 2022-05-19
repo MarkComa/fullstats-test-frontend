a53356ea4d09f6788b4f8d3002c879743158e988
markcoma <dex.go@yandex.ru>
2022-05-19 23:32:15 +0300

    type action

--- store/reducers/authSlice.ts
+++ store/reducers/authSlice.ts
@@ -1,11 +1,11 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 type State = {
 	email: string;
 	password: string;
 	isAuth: boolean;
-	access?: string;
-	refresh?: string;
+	access?: string | null;
+	refresh?: string | null;
 };
 const initialState: State = {
 	email: "",
@@ -17,13 +17,13 @@ const authSlice = createSlice({
 	name: "auth",
 	initialState,
 	reducers: {
-		login(state, action) {
+		login(state, action: PayloadAction<{access: string, refresh: string}>) {
       state.access = action.payload.access,
-	    state.refresh = action.payload.refresh,
+	    state.refresh = action.payload.refresh
     },
 		logout(state): void {
-			state.access = undefined,
-	    state.refresh = undefined,
+			state.access = null,
+	    state.refresh = null,
 			localStorage.removeItem("token");
 		},
 	},
